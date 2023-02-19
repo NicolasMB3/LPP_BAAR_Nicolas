d79e85d5af284c957ca282ddbf348dad6e5d8a25
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2023-02-19 18:07:29 +0100

    Refactoring some code

--- scripts/extra/navbar.js
+++ scripts/extra/navbar.js
@@ -1,4 +1,4 @@
-// enable bootstrap tooltips
+// Enable bootstrap tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
@@ -6,10 +6,10 @@ const links = document.querySelectorAll('#navigator a');
 
 links.forEach((link) => {
    link.addEventListener('mouseover', () => {
-      // set the 'active' class on the hovered element
+      // Set the 'active' class on the hovered element
       link.classList.add('active');
 
-      // remove the 'active' class from all other elements
+      // Remove the 'active' class from all other elements
       links.forEach((otherLink) => {
          if (otherLink !== link) {
             otherLink.classList.remove('active');
--- scripts/models/Recipes.js
+++ scripts/models/Recipes.js
@@ -10,28 +10,29 @@ class RecipeList {
 
    boucleArray() {
       for (let i = 0; i < this.recipes.length; i++) {
-         for (let j = 0; j < this.recipes[i].ingredients.length; j++) {
-            this.ingredients.push(this.recipes[i].ingredients[j].ingredient.toLowerCase());
+         // Get element for recipes
+         const ingredientList = this.recipes[i].ingredients,
+            ustensilsList = this.recipes[i].ustensils;
+
+         // Create array for ingredients, quantity and unit
+         let ingredients = [],
+            quantity = [],
+            unit = [];
+
+         for (let b = 0; b < ustensilsList.length; b++) {
+            this.ustensils.push(ustensilsList[b].toLowerCase());
          }
 
-         for (let j = 0; j < this.recipes[i].ustensils.length; j++) {
-            this.ustensils.push(this.recipes[i].ustensils[j].toLowerCase());
+         for (let c = 0; c < ingredientList.length; c++) {
+            ingredients.push(ingredientList[c].ingredient || '');
+            quantity.push(ingredientList[c].quantity || 'x');
+            unit.push(ingredientList[c].unit || '');
+            this.ingredients.push(ingredientList[c].ingredient.toLowerCase());
          }
 
          this.appliances.push(this.recipes[i].appliance.toLowerCase());
 
-         // Add ingredients, quantity and unit in arrays to create card item
-         let ingredients = [];
-         let quantity = [];
-         let unit = [];
-
-         for (let j = 0; j < this.recipes[i].ingredients.length; j++) {
-            ingredients.push(this.recipes[i].ingredients[j].ingredient);
-            quantity.push(this.recipes[i].ingredients[j].quantity || 'x');
-            unit.push(this.recipes[i].ingredients[j].unit || '');
-         }
-
-         // create card item for each recipe
+         // Create card item for each recipe
          let card = new CreateCard(this.recipes[i].name, this.recipes[i].time || '< 1', this.recipes[i].description, ingredients, quantity, unit);
          card.createCardItem();
       }
